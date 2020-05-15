2f14331605a8818cc8bef050f2b05965c9fa10ea
maryamz7 <61447737+maryamz7@users.noreply.github.com>
2020-05-14 22:36:08 -0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,82 @@
+
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:  [ '1281px',  '1680px' ],
+			large:   [ '981px',   '1280px' ],
+			medium:  [ '737px',   '980px'  ],
+			small:   [ null,      '736px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Dropdowns.
+		$('#nav > ul').dropotron({
+			mode: 'fade',
+			noOpenerFade: true,
+			alignment: 'center'
+		});
+
+	// Nav.
+
+		// Title Bar.
+			$(
+				'<div id="titleBar">' +
+					'<a href="#navPanel" class="toggle"></a>' +
+				'</div>'
+			)
+				.appendTo($body);
+
+		// Panel.
+			$(
+				'<div id="navPanel">' +
+					'<nav>' +
+						$('#nav').navList() +
+					'</nav>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'left',
+					target: $body,
+					visibleClass: 'navPanel-visible'
+				});
+
+})(jQuery);
+
+// Disable Google Maps scrolling
+// See http://stackoverflow.com/a/25904582/1607849
+// Disable scroll zooming and bind back the click event
+var onMapMouseleaveHandler = function(event) {
+	var that = $(this);
+	that.on('click', onMapClickHandler);
+	that.off('mouseleave', onMapMouseleaveHandler);
+	that.find('iframe').css("pointer-events", "none");
+  }
+  var onMapClickHandler = function(event) {
+	var that = $(this);
+	// Disable the click handler until the user leaves the map area
+	that.off('click', onMapClickHandler);
+	// Enable scrolling zoom
+	that.find('iframe').css("pointer-events", "auto");
+	// Handle the mouse leave event
+	that.on('mouseleave', onMapMouseleaveHandler);
+  }
+  // Enable map zooming with mouse scroll when the user clicks the map
+  $('.map').on('click', onMapClickHandler);
+  
